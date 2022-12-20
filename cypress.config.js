a9bad19d8a43eb8cfd594352f8fba11e0dd4e42d
Baldwin John Sumatra <baldwin.sumatra@heroteams.com.au>
2022-12-20 16:43:34 +0800

    remove failing spec

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,13 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'a7bq2k',
+  projectId: "a7bq2k",
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
-baseUrl: 'https://google.com',
+    baseUrl: "https://google.com",
     viewportWidth: 1920,
     viewportHeight: 1080,
     specPattern: "cypress/integration/**/*.spec.js",
-    setupNodeEvents(on, config) {
-    }
-  }
-});	
+  },
+});
